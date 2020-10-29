94871ad56ef1c38bc9917ab368dcd3d8bbb9c460
sebax774 <bugansebastian@gmail.com>
2020-10-29 11:41:24 +0100

    vypracovanie

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,20 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            string ret = "";
+            int length = original.Length;
+            if (length == 0)
+                return original;
+            else if (length == 1)
+                return original;
+            else
+            {
+                for (int i = 1; i < (length - 1); i++)
+                {
+                    ret = ret + original[i];
+                }
+                return ret;
+            }
         }
     }
 }
